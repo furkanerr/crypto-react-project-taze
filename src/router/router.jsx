452d6beadbae02ae1b25aa1fae65111366c14f4a
furkaner <furkan08_@hotmail.com>
2022-07-02 04:34:34 +0300

    Create router.jsx

---
+++ src/router/router.jsx
@@ -0,0 +1,21 @@
+
+/** Dependencies */
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+/** Components  */
+
+import HomePage from '../pages/HomePage/HomePage'
+import CoinDetail from '../pages/CoinDetailPage/CoinDetail'
+
+function Routers() {
+    return (
+
+     <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path='/coinDetail:id' element={<CoinDetail/>} />
+     </Routes>
+    )
+  }
+  
+  export default Routers
