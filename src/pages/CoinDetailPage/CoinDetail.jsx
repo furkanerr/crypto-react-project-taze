bced974e2fa76c54eecd9785a77fd9fdd8d81fdd
furkaner <furkan08_@hotmail.com>
2022-07-02 04:36:19 +0300

    Create CoinDetail.jsx

---
+++ src/pages/CoinDetailPage/CoinDetail.jsx
@@ -0,0 +1,14 @@
+import React,{useEffect,useState} from 'react'
+import {useParams} from 'react-router-dom'
+
+const CoinDetail = () => {
+    const {coinId} = useParams();
+    
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default CoinDetail
