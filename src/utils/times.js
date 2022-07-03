80001df90273cd2267141445e65d4d0c17d4212d
furkaner <furkan08_@hotmail.com>
2022-07-03 05:22:44 +0300

    times for chart

---
+++ src/utils/times.js
@@ -0,0 +1,23 @@
+export const times = [
+    {
+        id: 1,
+        name: '1 Day',
+        value: 1
+    },
+    {
+        id: 2,
+        name: '7 Days',
+        value: 7
+    },
+    {
+        id: 3,
+        name: '1 Month',
+        value: 30
+    },
+    {
+        id: 4,
+        name: '1 Year',
+        value: 365
+    }
+
+]
