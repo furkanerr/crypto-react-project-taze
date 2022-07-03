9230a672fdf3783bd43a8ea656ea8037f95cae6c
furkaner <furkan08_@hotmail.com>
2022-07-04 00:29:03 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useCoin } from '../../context/coinContext'
 import styles from './Header.module.css'
 const Header = () => {
+  const {openSideBar,setOpenSideBar} =useCoin()
   return (
     <div className={styles.header}>
       <Link to='/'>
     <h1>Crypto Currency Tracker</h1>
     </Link>
+    <div className={styles.openClosedSideBar} onClick={()=>
+      {
+        setOpenSideBar(!openSideBar)
+        console.log(openSideBar)
+      }}>View List</div>
   </div>
   )
 }
