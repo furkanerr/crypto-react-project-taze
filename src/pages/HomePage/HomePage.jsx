e31b98a2edfcbc5182b7005917ff422ce4c4a113
furkaner <furkan08_@hotmail.com>
2022-07-03 05:22:17 +0300

    search bar added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,46 +1,56 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./HomePage.module.css";
 import CoinCard from "../../components/CoinCardComponent/CoinCard";
 import Api from "../../services/api";
 import Pagination from "../../components/Pagination/Pagination";
-const HomePage = () => {
-    const [coins, setCoins] = useState([]);
-    const [page, setPage] = useState(1);
 
-    const fetchData = async () => {
-        const response = await Api.get(page);
-        setCoins(response.data.data);
-    }
-    coinImage,coinName,coinPrice,priceChange,volume
-    useEffect(() => {
-        fetchData();
-    }, [page]);
+import Header from "../../components/Header/Header";
+import { MoonLoader } from "react-spinners";
+const HomePage = () => {
+  const [coins, setCoins] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchData = async () => {
+    const response = await Api.getAllCoins(page);
+    setCoins(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  let filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
 
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <h1>Crypto Currency Tracker</h1>
+      <Header />
+      <div className={styles.searchBar}>
+        <input type="text" placeholder="Search..." onChange={(e)=>setSearchTerm(e.target.value)}/>
       </div>
-      <div className={styles.coinCards}>
-        {
-            coins.map(coin => {
-                return <CoinCard 
-                key={coin.id} 
-                coinName={coin.name} 
-                coinImage={coin.image}
-                coinPrice={coin.current_price}
-                priceChange={coin.price_change_percentage_24h}
-                volume={coin.total_volume}
-                coinId={coin.id}
-                 />
-            })
-            
-        }
+      {isLoading ? (
+        <MoonLoader color="#7b9f05" />
+      ) : (
+        <div className={styles.coinCards}>
+          {filteredCoins.map((coin) => (
+            <CoinCard
+              key={coin.id}
+              coinName={coin.name}
+              coinImage={coin.image}
+              coinPrice={coin.current_price}
+              priceChange={coin.price_change_percentage_24h}
+              volume={coin.total_volume}
+              coinId={coin.id}
+            />
+          ))}
+        </div>
+      )}
+
+      <div className={styles.pagination}>
+        <Pagination setPage={setPage} page={page} />
       </div>
-        <div className={styles.pagination}>
-            <Pagination setPage={setPage} page={page}/>
-        </div>    
-        
     </div>
   );
 };
