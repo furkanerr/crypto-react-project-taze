460c7b67d0ce8f2ee42aa7a0df03cbc88cc8ce1b
furkaner <furkan08_@hotmail.com>
2022-07-03 05:20:08 +0300

    coin card

--- src/components/CoinCardComponent/CoinCard.jsx
+++ src/components/CoinCardComponent/CoinCard.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import styles from "./CoinCard.module.css";
-import { Link } from "react-router-dom";
+import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 const CoinCard = ({
   coinImage,
   coinName,
   coinPrice,
   priceChange,
   volume,
-  coinId,
+  coinId
 }) => {
+  
   return (
+    
     <div className={styles.Card}>
-      <Link to={`/coinDetail/${coinId}`}>
+      <div className={styles.addToTheFavorites}>
+      <FontAwesomeIcon icon={faStar} style={{color:'#ffd43b'}}/>
+      </div>
+      <Link to={`/coinDetail/${coinId}`} >
         <div className={styles.coinImage}>
           <img src={coinImage} alt={coinName} />
         </div>
@@ -19,16 +26,17 @@ const CoinCard = ({
           <div>{coinName}</div>
         </div>
         <div className={styles.coinPrice}>
-          <div>{coinPrice}</div>
+          <div>${coinPrice}</div>
         </div>
-        <div className={styles.priceChange}>
-          <div>{priceChange}</div>
+        <div className={styles.priceChanges}>
+          <div style={priceChange.toString().includes('-')?{color:'red'}:{color:'green'}}>%{priceChange}</div>
         </div>
         <div className={styles.volume}>
-          <div>Volume:{volume}</div>
+          <div>Volume:${parseInt(volume).toLocaleString()}</div>
         </div>
-      </Link>
+        </Link>
     </div>
+ 
   );
 };
 
