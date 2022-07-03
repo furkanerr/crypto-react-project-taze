eca838b3bf72afdfbef5f7f651d9e849a2c28c07
furkaner <furkan08_@hotmail.com>
2022-07-04 00:26:33 +0300

    context added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Routers from './router/router.jsx';
 import './App.css';
-
+import { CoinProvider } from './context/coinContext.jsx';
 function App() {
   return (
     <div className="App">
+      <CoinProvider>
      <Routers/>
+      </CoinProvider>
     </div>
   );
 }
