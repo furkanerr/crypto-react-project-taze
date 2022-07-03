d0eb28607d3bdbf4f600811ae9e75ed85d4e016b
furkaner <furkan08_@hotmail.com>
2022-07-03 05:21:08 +0300

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import styles from './Header.module.css'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={styles.header}>
+      <Link to='/'>
+    <h1>Crypto Currency Tracker</h1>
+    </Link>
+  </div>
   )
 }
 
