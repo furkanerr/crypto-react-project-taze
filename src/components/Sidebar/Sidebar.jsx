777359234b69c84b8ed67a336a3242075e54519c
furkaner <furkan08_@hotmail.com>
2022-07-04 00:30:23 +0300

    side bar created
    react.memo used to avoid re-renders after input changes

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import MyChart from '../Chart/MyChart'
+import styles from './Sidebar.module.css'
+import { useCoin } from '../../context/coinContext'
+import ViewList from '../ViewListComponent/ViewList'
+import { Link } from 'react-router-dom'
+const Sidebar = () => {
+
+    const { selectedCoins,removeCoin} = useCoin()
+         //   debugger
+        console.log(selectedCoins)
+
+  return (
+    <div className={styles.container}>
+        <div className={styles.coinCard}>
+          {
+            selectedCoins.map(coin => (
+                <>
+                 <span onClick={()=>
+                  {  console.log(coin.coin.id)
+                    removeCoin(coin.coin.id)}} >X</span>
+                <Link to={`/coinDetail/${coin.coin.id}`}>
+               
+                <ViewList coin={coin.coin} />
+                <div style={{width:'95%'}}>
+                <MyChart data={coin.coinHistory} day={1} />
+                </div>
+                </Link>
+                </>
+            ))
+          }
+        </div>
+        
+    </div>
+  )
+}
+// arama yapılırken tekrar renderi etmeyi engellemek için
+export default React.memo(Sidebar)
